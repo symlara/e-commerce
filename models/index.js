e9b94d3aa414e308c798b6d9a18bc9de13ab1e0a
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-03-16 15:26:16 -0500

    fixed Product tag association in models index file

--- models/index.js
+++ models/index.js
@@ -17,7 +17,7 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'tagsIds',
+  as: 'tagIds',
   foreignKey: 'product_id'
 });
 
